1f278c0145700b5e9e401bf34f3e5573f79166cd
Florian Brunner <fbrunnerreg@gmx.ch>
2020-04-08 22:17:05 +0200

    - clean-up - removed drombler-media-importer classes

--- src/main/java/org/drombler/event/management/EventManager.java
+++ src/main/java/org/drombler/event/management/EventManager.java
@@ -20,7 +20,6 @@ import java.util.stream.Stream;
 import org.drombler.event.core.Event;
 import org.drombler.event.core.EventDuration;
 import org.drombler.event.core.FullTimeEventDuration;
-import org.drombler.media.importing.ImportEventDurationComparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,54 +32,54 @@ public class EventManager {
     private static final Logger LOG = LoggerFactory.getLogger(EventManager.class);
 
     private final Map<LocalDate, SortedSet<Event>> events = new HashMap<>();
-    private final Comparator<EventDuration> eventDurationComparator = new ImportEventDurationComparator();
-    private final Comparator<Event> eventComparator = Comparator.comparing(Event::getDuration, eventDurationComparator)
-            .thenComparing(Event::getName);
+//    private final Comparator<EventDuration> eventDurationComparator = new ImportEventDurationComparator();
+//    private final Comparator<Event> eventComparator = Comparator.comparing(Event::getDuration, eventDurationComparator)
+//            .thenComparing(Event::getName);
 
     public EventManager() throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(EventManager.class.getResourceAsStream("media-event-dir-paths.txt"), Charset.forName("UTF-8")))) {
-            br.lines()
-                    .map(Paths::get)
-                    .forEach(this::updateEventMap);
-        }
+//        try (BufferedReader br = new BufferedReader(new InputStreamReader(EventManager.class.getResourceAsStream("media-event-dir-paths.txt"), Charset.forName("UTF-8")))) {
+//            br.lines()
+//                    .map(Paths::get)
+//                    .forEach(this::updateEventMap);
+//        }
     }
 
-    public void updateEventMap(Path basePath) {
-        try (final Stream<Path> paths = Files.list(basePath)) {
-            paths.filter(Files::isDirectory).map((Path path) -> Event.fullTimeDayEvent(path.getFileName().toString()))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .forEach(this::updateEventMap);
-        } catch (IOException ex) {
-            LOG.error(ex.getMessage(), ex);
-        }
-    }
-
-    private void updateEventMap(Event event) {
-        final FullTimeEventDuration duration = (FullTimeEventDuration) event.getDuration();
-        for (LocalDate date = duration.getStartDateInclusive(); date.isBefore(duration.getEndDateInclusive()) || date.equals(duration.getEndDateInclusive()); date = date.plusDays(1)) {
-            if (!events.containsKey(date)) {
-                events.put(date, new TreeSet<>(eventComparator));
-            }
-            if (!events.get(date).contains(event)) {
-                events.get(date).add(event);
-                LOG.debug(event.getName() + " - " + event.getDirName());
-            }
-        }
-    }
-
-    public Event getFirstEvent(LocalDate date) {
-        updateEventMap(createEvent(date));
-        return events.get(date).first();
-    }
-
-    private Event createEvent(LocalDate date) {
-        return new Event(null, "", new FullTimeEventDuration(date, date));
-    }
-
-    public SortedSet<Event> getAllEvents() {
-        return events.values().stream()
-                .flatMap(Collection::stream)
-                .collect(Collectors.toCollection(() -> new TreeSet<>(eventComparator)));
-    }
+//    public void updateEventMap(Path basePath) {
+//        try (final Stream<Path> paths = Files.list(basePath)) {
+//            paths.filter(Files::isDirectory).map((Path path) -> Event.fullTimeDayEvent(path.getFileName().toString()))
+//                    .filter(Optional::isPresent)
+//                    .map(Optional::get)
+//                    .forEach(this::updateEventMap);
+//        } catch (IOException ex) {
+//            LOG.error(ex.getMessage(), ex);
+//        }
+//    }
+//
+//    private void updateEventMap(Event event) {
+//        final FullTimeEventDuration duration = (FullTimeEventDuration) event.getDuration();
+//        for (LocalDate date = duration.getStartDateInclusive(); date.isBefore(duration.getEndDateInclusive()) || date.equals(duration.getEndDateInclusive()); date = date.plusDays(1)) {
+//            if (!events.containsKey(date)) {
+//                events.put(date, new TreeSet<>(eventComparator));
+//            }
+//            if (!events.get(date).contains(event)) {
+//                events.get(date).add(event);
+//                LOG.debug(event.getName() + " - " + event.getDirName());
+//            }
+//        }
+//    }
+//
+//    public Event getFirstEvent(LocalDate date) {
+//        updateEventMap(createEvent(date));
+//        return events.get(date).first();
+//    }
+//
+//    private Event createEvent(LocalDate date) {
+//        return new Event(null, "", new FullTimeEventDuration(date, date));
+//    }
+//
+//    public SortedSet<Event> getAllEvents() {
+//        return events.values().stream()
+//                .flatMap(Collection::stream)
+//                .collect(Collectors.toCollection(() -> new TreeSet<>(eventComparator)));
+//    }
 }
--- src/main/java/org/drombler/media/core/photo/PhotoStorage.java
+++ src/main/java/org/drombler/media/core/photo/PhotoStorage.java
@@ -5,20 +5,8 @@
  */
 package org.drombler.media.core.photo;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-import org.drombler.event.core.Event;
-import org.drombler.identity.core.DromblerIdentityProviderManager;
-import org.drombler.identity.core.DromblerUserId;
-import org.drombler.media.core.AbstractMediaOrganizer;
 import org.drombler.media.core.AbstractMediaStorage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
